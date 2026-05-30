0d1bcd30f66a66dfb3117ed2dae00765accec6d3
Ahmed Gamal <ahmedgamal0316@gmail.com>
2026-05-30 16:04:55 +0300

    add app code

---
+++ src/app.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "========================================="
+echo "🚀 Hello from the new DevOps Project!"
+echo "The application is running successfully."
+echo "========================================="
